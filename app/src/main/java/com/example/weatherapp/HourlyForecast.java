30064eecf1f7803aff4eab5055cb4ff34116ab12
DESKTOP-SG5OL0L\xuant <xuanthuc123412@gmail.com>
2024-10-23 01:06:10 +0700

    Thuc_update_UIhome_and_apitimeforecast

--- app/src/main/java/com/example/weatherapp/HourlyForecast.java
+++ app/src/main/java/com/example/weatherapp/HourlyForecast.java
@@ -1,37 +1,14 @@
 package com.example.weatherapp;
-
-//public class HourlyForecast {
-//    private String temperature;
-//    private String time;
-//    private int iconResourceId;
-//
-//    public HourlyForecast(String temperature, String time, int iconResourceId) {
-//        this.temperature = temperature;
-//        this.time = time;
-//        this.iconResourceId = iconResourceId;
-//    }
-//
-//    public String getTemperature() {
-//        return temperature;
-//    }
-//
-//    public String getTime() {
-//        return time;
-//    }
-//
-//    public int getIconResourceId() {
-//        return iconResourceId;
-//    }
-//}
+//Thuc update time
 public class HourlyForecast {
     private final String temperature;
     private final String description;
-    private final String time; // Đảm bảo rằng kiểu dữ liệu là String
+    private final String time;
 
     public HourlyForecast(String temperature, String description, int time) {
         this.temperature = temperature;
         this.description = description;
-        this.time = String.valueOf(time);  // Nếu là int, hãy sửa thành String
+        this.time = String.valueOf(time);
     }
 
     public String getTemperature() {
