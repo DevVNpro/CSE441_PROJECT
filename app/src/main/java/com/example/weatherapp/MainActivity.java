0878ac5b39288c2a6af2bcbcac14eee2b9feaf72
thuongpower <thuongpower9122003@gmail.com>
2024-10-23 16:33:19 +0700

    Update

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
