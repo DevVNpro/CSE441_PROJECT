b19af2ad7507fdc54d4fea5d3651f5e8b86f3484
VKien <kien1652003@gmail.com>
2024-11-02 09:16:44 +0700

    updateSearch

--- app/src/main/java/com/example/weatherapp/CitySearch.java
+++ app/src/main/java/com/example/weatherapp/CitySearch.java
@@ -1,4 +1,39 @@
 package com.example.weatherapp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SearchView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class CitySearch {
+public class CitySearch extends AppCompatActivity {
+
+    private SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_search); // Đảm bảo bạn có layout này
+
+        searchView = findViewById(R.id.searchbar);
+        setupSearchView();
+    }
+
+    private void setupSearchView() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Khi người dùng nhấn Enter
+                Intent intent = new Intent(CitySearch.this, SearchItemActivity.class);
+                intent.putExtra("city_name", query);
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Tùy chọn: Xử lý khi văn bản thay đổi
+                return false;
+            }
+        });
+    }
 }
